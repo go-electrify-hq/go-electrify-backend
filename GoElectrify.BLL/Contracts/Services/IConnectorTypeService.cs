4b5cd90ded80e7a480515cba15a478094974641d
Bao Nguyen <nbn1784@gmail.com>
2025-10-01 01:21:10 +0700

    feat(bll): Introduce ConnectorType core contracts and DTOs

---
+++ GoElectrify.BLL/Contracts/Services/IConnectorTypeService.cs
@@ -0,0 +1,18 @@
+﻿using GoElectrify.BLL.Dto.ConnectorTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoElectrify.BLL.Contracts.Services
+{
+    public interface IConnectorTypeService
+    {
+        Task<List<ConnectorTypeDto>> ListAsync(string? search, CancellationToken ct);
+        Task<ConnectorTypeDto?> GetAsync(int id, CancellationToken ct);
+        Task<int> CreateAsync(CreateConnectorTypeDto dto, CancellationToken ct);
+        Task UpdateAsync(int id, UpdateConnectorTypeDto dto, CancellationToken ct);
+        Task DeleteAsync(int id, CancellationToken ct);
+    }
+}
