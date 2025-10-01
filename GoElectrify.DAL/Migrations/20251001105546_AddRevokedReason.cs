81503b738ae2712609bb98394494d42b5c8cbce7
Ivorlee <dnhuy207@gmail.com>
2025-10-01 17:57:05 +0700

    Add Revoked Reason

---
+++ GoElectrify.DAL/Migrations/20251001105546_AddRevokedReason.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GoElectrify.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRevokedReason : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "RevokedReason",
+                table: "StationStaff",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RevokedReason",
+                table: "StationStaff");
+        }
+    }
+}
