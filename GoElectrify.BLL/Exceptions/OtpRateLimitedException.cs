935f9101b6d290fefed9f5b51251f545a5ebac8f
Ivorlee <dnhuy207@gmail.com>
2025-10-17 18:39:04 +0700

    Fix Author

---
+++ GoElectrify.BLL/Exceptions/OtpRateLimitedException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoElectrify.BLL.Exceptions
+{
+    public sealed class OtpRateLimitedException : Exception
+    {
+        public int RetryAfterSeconds { get; }
+        public OtpRateLimitedException(int retryAfterSeconds)
+            : base("Too many OTP requests. Try again later.") => RetryAfterSeconds = retryAfterSeconds;
+
+        public OtpRateLimitedException(string message, int retryAfterSeconds)
+            : base(message) => RetryAfterSeconds = retryAfterSeconds;
+
+        public OtpRateLimitedException(string message, Exception? inner, int retryAfterSeconds)
+            : base(message, inner) => RetryAfterSeconds = retryAfterSeconds;
+    }
+}
