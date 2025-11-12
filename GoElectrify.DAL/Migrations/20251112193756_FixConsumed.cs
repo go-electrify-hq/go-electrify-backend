3c75f786224247b2d25c3cdb6bf127c08018a391
qbao0111 <hoangquocbao011105@gmail.com>
2025-11-13 02:38:20 +0700

    Fix Migration

---
+++ GoElectrify.DAL/Migrations/20251112193756_FixConsumed.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GoElectrify.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixConsumed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "ix_bookings_charger_id_scheduled_start",
+                table: "Bookings");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_bookings_charger_id_scheduled_start",
+                table: "Bookings",
+                columns: new[] { "charger_id", "scheduled_start" },
+                unique: true,
+                filter: "charger_id IS NOT NULL AND status IN ('PENDING','CONFIRMED')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "ix_bookings_charger_id_scheduled_start",
+                table: "Bookings");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_bookings_charger_id_scheduled_start",
+                table: "Bookings",
+                columns: new[] { "charger_id", "scheduled_start" },
+                unique: true,
+                filter: "charger_id IS NOT NULL AND status IN ('PENDING','CONFIRMED','CONSUMED')");
+        }
+    }
+}
